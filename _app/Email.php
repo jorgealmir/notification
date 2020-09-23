ecffb012722862c4ee3f413b7cfcd6cdb2a36a56
jorgealmir <jorgealmirmartins@gmail.com>
2020-09-22 22:05:08 -0300

    Desenvolvendo aplicação e atualização do phpmailer

--- _app/Email.php
+++ _app/Email.php
@@ -2,16 +2,49 @@
 
 namespace Notification;
 
+use Exception;
+use stdClass;
 use PHPMailer\PHPMailer\PHPMailer;
 
 class Email 
 {
+    /** @var stdClass */
+    private $mail;
+
     public function __construct() {
-        $email = new PHPMailer;
+        $this->mail = new PHPMailer(true);
+//        $this->mail->SMTPDebug = 2;
+        $this->mail->isSMTP();
+        $this->mail->Host = 'smtpi.kinghost.net';
+        $this->mail->SMTPAuth = true;
+        $this->mail->Username = 'jorgealmir@drevekcontabil.com.br';
+        $this->mail->Password = 'Fortuito1964';
+        $this->mail->SMTPSecure = "tls";
+        $this->mail->Port = 587;
+        $this->mail->CharSet = "utf-8";
+        $this->mail->setLanguage("br");
+        $this->mail->isHTML();
+        $this->mail->setFrom('jorgealmir@drevekcontabil.com.br', 'Jorge Almir Martins');
     }
     
-    public function sendMail() 
-    {
-        echo "E-mail enviado!";
+    public function sendMail(
+        $subject, 
+        $body, 
+        $replyEmail, 
+        $replyName, 
+        $addressEmail, 
+        $addressName
+    ) {
+        $this->mail->Subject = (string) $subject;
+        $this->mail->Body = $body;
+        
+        $this->mail->addReplyTo($replyEmail, $replyName);
+        $this->mail->addAddress($addressEmail, $addressName);
+        
+        try {
+            $this->mail->send();
+        } catch (Exception $e) {
+            echo "Erro ao enviar o e-mail: {$this->mail->ErrorInfo} {$e->getMessage()}";
+        }
     }
 }
--- index.php
+++ index.php
@@ -5,4 +5,13 @@ require __DIR__ . '/vendor/autoload.php';
 use Notification\Email;
 
 $novoEmail = new Email;
-$novoEmail->sendMail();
+$novoEmail->sendMail(
+    "Assunto de Teste",
+    "<p>Esse é um email de <b>teste</b></p>",
+    "jorge@drevekcontabil.com.br",
+    "Jorge (Reply)",
+    "jorgealmirmartins@gmail.com",
+    "Jorge (Destinatário)"
+);
+
+var_dump($novoEmail);
